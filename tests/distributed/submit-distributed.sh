dff8005412dd4401f7f1b795a6a33d47b3699571
frostedoyster <bigi.f@libero.it>
2024-06-17 13:31:38 +0200

    Add dependency on correct branch

--- tests/distributed/submit-distributed.sh
+++ tests/distributed/submit-distributed.sh
@@ -4,10 +4,8 @@
 #SBATCH --ntasks-per-node 2
 #SBATCH --gpus-per-node 2
 #SBATCH --cpus-per-task 8
-#SBATCH --exclusive
 #SBATCH --time=1:00:00
 
-module load gcc python
-source /home/bigi/virtualenv-i/bin/activate
+# Activate your modules, environments, containers etc here
 
 srun mtt train options-distributed.yaml
