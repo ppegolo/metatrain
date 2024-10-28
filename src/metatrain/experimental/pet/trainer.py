e8edb30c1f5996643ddf3240ffcccb6723358b51
Arslan Mazitov <arslan.mazitov@phystech.edu>
2024-10-28 16:34:40 +0100

    Temporary WIP version of the native PET trainer

--- src/metatrain/experimental/pet/trainer.py
+++ src/metatrain/experimental/pet/trainer.py
@@ -47,6 +47,29 @@ from ...utils.data import Dataset, check_datasets
 from . import PET as WrappedPET
 from .utils import dataset_to_ase, update_hypers
 
+from ...utils.distributed.slurm import DistributedEnvironment
+from ...utils.neighbor_lists import (
+    get_requested_neighbor_lists,
+    get_system_with_neighbor_lists,
+)
+from ...utils.distributed.distributed_data_parallel import DistributedDataParallel
+from torch.utils.data import DataLoader, DistributedSampler
+from ...utils.data import CombinedDataLoader, Dataset, TargetInfoDict, collate_fn
+from ...utils.data.extract_targets import get_targets_dict
+from ...utils.external_naming import to_external_name
+
+from ...utils.loss import TensorMapDictLoss
+
+from ...utils.metrics import MAEAccumulator, RMSEAccumulator
+from ...utils.transfer import (
+    systems_and_targets_to_device,
+    systems_and_targets_to_dtype,
+)
+from ...utils.additive import remove_additive
+from ...utils.evaluate_model import evaluate_model
+from ...utils.per_atom import average_by_num_atoms
+
+from ...utils.logging import MetricLogger
 
 logger = logging.getLogger(__name__)
 
@@ -67,6 +90,355 @@ class Trainer:
         checkpoint_dir: str,
     ):
         assert dtype in WrappedPET.__supported_dtypes__
+
+        is_distributed = self.hypers["distributed"]
+
+        if is_distributed:
+            distr_env = DistributedEnvironment(self.hypers["distributed_port"])
+            torch.distributed.init_process_group(backend="nccl")
+            world_size = torch.distributed.get_world_size()
+            rank = torch.distributed.get_rank()
+        else:
+            rank = 0
+
+        if is_distributed:
+            if len(devices) > 1:
+                raise ValueError(
+                    "Requested distributed training with the `multi-gpu` device. "
+                    " If you want to run distributed training with SOAP-BPNN, please "
+                    "set `device` to cuda."
+                )
+            # the calculation of the device number works both when GPUs on different
+            # processes are not visible to each other and when they are
+            device_number = distr_env.local_rank % torch.cuda.device_count()
+            device = torch.device("cuda", device_number)
+        else:
+            device = devices[
+                0
+            ]  # only one device, as we don't support multi-gpu for now
+
+        if is_distributed:
+            logger.info(f"Training on {world_size} devices with dtype {dtype}")
+        else:
+            logger.info(f"Training on device {device} with dtype {dtype}")
+
+        # Calculate the neighbor lists in advance (in particular, this
+        # needs to happen before the additive models are trained, as they
+        # might need them):
+        logger.info("Calculating neighbor lists for the datasets")
+        requested_neighbor_lists = get_requested_neighbor_lists(model)
+        for dataset in train_datasets + val_datasets:
+            for i in range(len(dataset)):
+                system = dataset[i]["system"]
+                # The following line attaches the neighbors lists to the system,
+                # and doesn't require to reassign the system to the dataset:
+                _ = get_system_with_neighbor_lists(system, requested_neighbor_lists)
+
+        # Move the model to the device and dtype:
+        model.to(device=device, dtype=dtype)
+        # The additive models of the SOAP-BPNN are always in float64 (to avoid
+        # numerical errors in the composition weights, which can be very large).
+        for additive_model in model.additive_models:
+            additive_model.to(dtype=torch.float64)
+
+        logger.info("Calculating composition weights")
+        model.additive_models[0].train_model(  # this is the composition model
+            train_datasets, self.hypers["fixed_composition_weights"]
+        )
+
+        if is_distributed:
+            model = DistributedDataParallel(model, device_ids=[device])
+
+        logger.info("Setting up data loaders")
+
+        if is_distributed:
+            train_samplers = [
+                DistributedSampler(
+                    train_dataset,
+                    num_replicas=world_size,
+                    rank=rank,
+                    shuffle=True,
+                    drop_last=True,
+                )
+                for train_dataset in train_datasets
+            ]
+            val_samplers = [
+                DistributedSampler(
+                    val_dataset,
+                    num_replicas=world_size,
+                    rank=rank,
+                    shuffle=False,
+                    drop_last=False,
+                )
+                for val_dataset in val_datasets
+            ]
+        else:
+            train_samplers = [None] * len(train_datasets)
+            val_samplers = [None] * len(val_datasets)
+
+        # Create dataloader for the training datasets:
+        train_dataloaders = []
+        for dataset, sampler in zip(train_datasets, train_samplers):
+            train_dataloaders.append(
+                DataLoader(
+                    dataset=dataset,
+                    batch_size=self.hypers["batch_size"],
+                    sampler=sampler,
+                    shuffle=(
+                        sampler is None
+                    ),  # the sampler takes care of this (if present)
+                    drop_last=(
+                        sampler is None
+                    ),  # the sampler takes care of this (if present)
+                    collate_fn=collate_fn,
+                )
+            )
+        train_dataloader = CombinedDataLoader(train_dataloaders, shuffle=True)
+
+        # Create dataloader for the validation datasets:
+        val_dataloaders = []
+        for dataset, sampler in zip(val_datasets, val_samplers):
+            val_dataloaders.append(
+                DataLoader(
+                    dataset=dataset,
+                    batch_size=self.hypers["batch_size"],
+                    sampler=sampler,
+                    shuffle=False,
+                    drop_last=False,
+                    collate_fn=collate_fn,
+                )
+            )
+        val_dataloader = CombinedDataLoader(val_dataloaders, shuffle=False)
+
+        # Extract all the possible outputs and their gradients:
+        train_targets = get_targets_dict(
+            train_datasets, (model.module if is_distributed else model).dataset_info
+        )
+        outputs_list = []
+        for target_name, target_info in train_targets.items():
+            outputs_list.append(target_name)
+            for gradient_name in target_info.gradients:
+                outputs_list.append(f"{target_name}_{gradient_name}_gradients")
+        # Create a loss weight dict:
+        loss_weights_dict = {}
+        for output_name in outputs_list:
+            loss_weights_dict[output_name] = (
+                self.hypers["loss_weights"][
+                    to_external_name(output_name, train_targets)
+                ]
+                if to_external_name(output_name, train_targets)
+                in self.hypers["loss_weights"]
+                else 1.0
+            )
+        loss_weights_dict_external = {
+            to_external_name(key, train_targets): value
+            for key, value in loss_weights_dict.items()
+        }
+        logging.info(f"Training with loss weights: {loss_weights_dict_external}")
+
+        # Create a loss function:
+        loss_fn = TensorMapDictLoss(loss_weights_dict)
+
+        # Create an optimizer:
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=self.hypers["learning_rate"]
+        )
+        if self.optimizer_state_dict is not None:
+            optimizer.load_state_dict(self.optimizer_state_dict)
+
+        # Create a scheduler:
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            factor=self.hypers["scheduler_factor"],
+            patience=self.hypers["scheduler_patience"],
+        )
+        if self.scheduler_state_dict is not None:
+            lr_scheduler.load_state_dict(self.scheduler_state_dict)
+
+        # counters for early stopping:
+        best_val_loss = float("inf")
+        epochs_without_improvement = 0
+
+        # per-atom targets:
+        per_structure_targets = self.hypers["per_structure_targets"]
+
+        # Log the initial learning rate:
+        old_lr = optimizer.param_groups[0]["lr"]
+        logger.info(f"Initial learning rate: {old_lr}")
+
+        start_epoch = 0 if self.epoch is None else self.epoch + 1
+
+        # Train the model:
+        logger.info("Starting training")
+        for epoch in range(start_epoch, start_epoch + self.hypers["num_epochs"]):
+            if is_distributed:
+                sampler.set_epoch(epoch)
+
+            train_rmse_calculator = RMSEAccumulator()
+            val_rmse_calculator = RMSEAccumulator()
+            if self.hypers["log_mae"]:
+                train_mae_calculator = MAEAccumulator()
+                val_mae_calculator = MAEAccumulator()
+
+            train_loss = 0.0
+            for batch in train_dataloader:
+                optimizer.zero_grad()
+
+                systems, targets = batch
+                systems, targets = systems_and_targets_to_device(
+                    systems, targets, device
+                )
+                for additive_model in (
+                    model.module if is_distributed else model
+                ).additive_models:
+                    targets = remove_additive(
+                        systems, targets, additive_model, train_targets
+                    )
+                systems, targets = systems_and_targets_to_dtype(systems, targets, dtype)
+                predictions = evaluate_model(
+                    model,
+                    systems,
+                    TargetInfoDict(
+                        **{key: train_targets[key] for key in targets.keys()}
+                    ),
+                    is_training=True,
+                )
+
+                # average by the number of atoms
+                predictions = average_by_num_atoms(
+                    predictions, systems, per_structure_targets
+                )
+                targets = average_by_num_atoms(targets, systems, per_structure_targets)
+
+                train_loss_batch = loss_fn(predictions, targets)
+
+                train_loss_batch.backward()
+                optimizer.step()
+
+                if is_distributed:
+                    # sum the loss over all processes
+                    torch.distributed.all_reduce(train_loss_batch)
+                train_loss += train_loss_batch.item()
+                train_rmse_calculator.update(predictions, targets)
+                if self.hypers["log_mae"]:
+                    train_mae_calculator.update(predictions, targets)
+
+            finalized_train_info = train_rmse_calculator.finalize(
+                not_per_atom=["positions_gradients"] + per_structure_targets,
+                is_distributed=is_distributed,
+                device=device,
+            )
+            if self.hypers["log_mae"]:
+                finalized_train_info.update(
+                    train_mae_calculator.finalize(
+                        not_per_atom=["positions_gradients"] + per_structure_targets,
+                        is_distributed=is_distributed,
+                        device=device,
+                    )
+                )
+
+            val_loss = 0.0
+            for batch in val_dataloader:
+                systems, targets = batch
+                systems, targets = systems_and_targets_to_device(
+                    systems, targets, device
+                )
+                for additive_model in (
+                    model.module if is_distributed else model
+                ).additive_models:
+                    targets = remove_additive(
+                        systems, targets, additive_model, train_targets
+                    )
+                systems, targets = systems_and_targets_to_dtype(systems, targets, dtype)
+                predictions = evaluate_model(
+                    model,
+                    systems,
+                    TargetInfoDict(
+                        **{key: train_targets[key] for key in targets.keys()}
+                    ),
+                    is_training=False,
+                )
+
+                # average by the number of atoms
+                predictions = average_by_num_atoms(
+                    predictions, systems, per_structure_targets
+                )
+                targets = average_by_num_atoms(targets, systems, per_structure_targets)
+
+                val_loss_batch = loss_fn(predictions, targets)
+
+                if is_distributed:
+                    # sum the loss over all processes
+                    torch.distributed.all_reduce(val_loss_batch)
+                val_loss += val_loss_batch.item()
+                val_rmse_calculator.update(predictions, targets)
+                if self.hypers["log_mae"]:
+                    val_mae_calculator.update(predictions, targets)
+
+            finalized_val_info = val_rmse_calculator.finalize(
+                not_per_atom=["positions_gradients"] + per_structure_targets,
+                is_distributed=is_distributed,
+                device=device,
+            )
+            if self.hypers["log_mae"]:
+                finalized_val_info.update(
+                    val_mae_calculator.finalize(
+                        not_per_atom=["positions_gradients"] + per_structure_targets,
+                        is_distributed=is_distributed,
+                        device=device,
+                    )
+                )
+
+            # Now we log the information:
+            finalized_train_info = {"loss": train_loss, **finalized_train_info}
+            finalized_val_info = {"loss": val_loss, **finalized_val_info}
+
+            if epoch == start_epoch:
+                metric_logger = MetricLogger(
+                    log_obj=logger,
+                    dataset_info=model.dataset_info,
+                    initial_metrics=[finalized_train_info, finalized_val_info],
+                    names=["training", "validation"],
+                )
+            if epoch % self.hypers["log_interval"] == 0:
+                metric_logger.log(
+                    metrics=[finalized_train_info, finalized_val_info],
+                    epoch=epoch,
+                    rank=rank,
+                )
+
+            lr_scheduler.step(val_loss)
+            new_lr = lr_scheduler.get_last_lr()[0]
+            if new_lr != old_lr:
+                logger.info(f"Changing learning rate from {old_lr} to {new_lr}")
+                old_lr = new_lr
+
+            if epoch % self.hypers["checkpoint_interval"] == 0:
+                if is_distributed:
+                    torch.distributed.barrier()
+                self.optimizer_state_dict = optimizer.state_dict()
+                self.scheduler_state_dict = lr_scheduler.state_dict()
+                self.epoch = epoch
+                if rank == 0:
+                    self.save_checkpoint(
+                        (model.module if is_distributed else model),
+                        Path(checkpoint_dir) / f"model_{epoch}.ckpt",
+                    )
+
+            # early stopping criterion:
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                epochs_without_improvement = 0
+            else:
+                epochs_without_improvement += 1
+                if epochs_without_improvement >= self.hypers["early_stopping_patience"]:
+                    logger.info(
+                        "Early stopping criterion reached after "
+                        f"{self.hypers['early_stopping_patience']} epochs "
+                        "without improvement."
+                    )
+                    break
 
         name_of_calculation = "pet"
         self.pet_dir = Path(checkpoint_dir) / name_of_calculation
